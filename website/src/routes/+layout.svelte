f1f82b18f217123b1f4f86214567d6becbcf6ab9
Josh Dales <git@joshdales.dev>
2023-11-16 17:48:55 -0500

    Put a footer in the layout

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -3,3 +3,29 @@
 </script>
 
 <slot />
+
+<footer>
+	<h3>
+		<a class="link-4" href="https://github.com/joshdales/tundra-css"
+			>github.com/joshdales/tundra-css</a
+		>
+	</h3>
+</footer>
+
+<style>
+	footer {
+		text-align: center;
+		padding-block: var(--space-4);
+	}
+
+	a {
+		position: relative;
+		color: var(--green);
+		text-decoration: none;
+		padding-block-end: var(--space-1);
+	}
+
+	a:hover {
+		text-decoration: underline;
+	}
+</style>

780b9b503273a9471c920e21b29bda208e1f9cc4
Josh Dales <git@joshdales.dev>
2023-11-22 08:16:58 -0500

    style the token selector

--- website/src/routes/typography/+page.svelte
+++ website/src/routes/typography/+page.svelte
@@ -4,10 +4,6 @@
 
 	let selectedToken = $state($page.url.searchParams.get('token') || '')
 
-	function updateSelectedToken(event: Event & { currentTarget: EventTarget & HTMLInputElement }) {
-		selectedToken = event.currentTarget.value
-	}
-
 	$effect(() => {
 		const token = $page.url.searchParams.get('token')
 		if (token && !selectedToken) {
@@ -30,51 +26,55 @@
 	<fieldset name="typography">
 		<legend class="alt-heading-3">Token type</legend>
 
-		<label class="label-4" for="token-font-weight">
+		<label class:active={selectedToken === 'font-weight'} class="label-4" for="token-font-weight">
 			Font Weight
 			<input
 				bind:group={selectedToken}
-				on:change={updateSelectedToken}
 				type="radio"
 				name="typography"
 				value="font-weight"
 				id="token-font-weight"
+				hidden
 			/>
 		</label>
 
-		<label class="label-4" for="token-font-size">
+		<label class:active={selectedToken === 'font-size'} class="label-4" for="token-font-size">
 			Font Size
 			<input
 				bind:group={selectedToken}
-				on:change={updateSelectedToken}
 				type="radio"
 				name="typography"
 				value="font-size"
 				id="token-font-size"
+				hidden
 			/>
 		</label>
 
-		<label class="label-4" for="token-line-height">
+		<label class:active={selectedToken === 'line-height'} class="label-4" for="token-line-height">
 			Line Height
 			<input
 				bind:group={selectedToken}
-				on:change={updateSelectedToken}
 				type="radio"
 				name="typography"
 				value="line-height"
 				id="token-line-height"
+				hidden
 			/>
 		</label>
 
-		<label class="label-4" for="token-letter-spacing">
+		<label
+			class:active={selectedToken === 'letter-spacing'}
+			class="label-4"
+			for="token-letter-spacing"
+		>
 			Letter Spacing
 			<input
 				bind:group={selectedToken}
-				on:change={updateSelectedToken}
 				type="radio"
 				name="typography"
 				value="letter-spacing"
 				id="token-letter-spacing"
+				hidden
 			/>
 		</label>
 	</fieldset>
@@ -84,10 +84,38 @@
 
 <style>
 	fieldset {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		gap: var(--space-6);
 		margin-block-start: var(--space-4);
-		display: flex;
-		justify-content: space-evenly;
 		border: none;
 		padding: 0;
+	}
+
+	label {
+		cursor: pointer;
+		color: var(--foreground);
+		border-radius: var(--radius-2);
+		padding-block: var(--space-4);
+		padding-inline: var(--space-6);
+		border-top: 0.5px solid transparent;
+		text-align: center;
+		transition:
+			all 0.1s ease-in-out,
+			transform 0.3s ease-in;
+	}
+
+	.active,
+	label:hover {
+		border-color: var(--blue-8);
+		background-color: oklch(from var(--blue-8) l c h / 40%);
+		box-shadow:
+			0 var(--space-1) var(--space-2) oklch(from var(--neutral-1) l c h / 5%),
+			0 0 var(--space-1) oklch(from var(--neutral-1) l c h / 10%),
+			inset var(--space-2) var(--space-1) var(--space-3) oklch(from var(--blue-7) l c h / 10%);
+	}
+
+	label:hover {
+		transform: translate(0, calc(var(--space-1) * -1));
 	}
 </style>

8177ee550014ba99d6f110df03e84a2fb2f398cf
Josh Dales <git@joshdales.dev>
2023-12-05 16:09:25 -0500

    Get copy notification working on the colour page

--- website/src/routes/colours/+page.svelte
+++ website/src/routes/colours/+page.svelte
@@ -1,10 +1,28 @@
 <script lang="ts">
+	import { fly } from 'svelte/transition'
 	import DesignToken from '$lib/components/design_token.svelte'
+	import Toast from '$lib/components/toast.svelte'
+	import { onDestroy } from 'svelte'
 	const colourMap = ['neutral', 'red', 'green', 'blue', 'yellow'].map((colour) =>
 		new Array(8).fill(colour),
 	)
 
-	// TODO: Handle the clicked event to display a notification
+	let copiedColour: { value: string; success: boolean } | undefined
+	let timer: number
+	$: {
+		if (copiedColour && copiedColour.success) {
+			if (timer) {
+				clearTimeout(timer)
+			}
+			timer = setTimeout(() => {
+				copiedColour = undefined
+			}, 2000)
+		}
+	}
+
+	onDestroy(() => {
+		clearTimeout(timer)
+	})
 </script>
 
 <main>
@@ -18,7 +36,11 @@
 	{#each colourMap as colour}
 		<div class="colour">
 			{#each colour as item, index}
-				<DesignToken property={item} level={index + 1} on:copied_value>
+				<DesignToken
+					property={item}
+					level={index + 1}
+					on:copied_value={(ev) => (copiedColour = ev.detail)}
+				>
 					<div class="colour-block" />
 				</DesignToken>
 			{/each}
@@ -26,8 +48,26 @@
 	{/each}
 </main>
 
+{#key copiedColour}
+	<div class="toaster" transition:fly={{ y: 100, duration: 500 }}>
+		{#if copiedColour}
+			<Toast type={copiedColour.success ? 'success' : 'danger'} showClose={!copiedColour.success}>
+				{#if copiedColour.success}
+					<p class="body-2">
+						<code>{copiedColour.value}</code> copied to your clipboard!
+					</p>
+				{:else}
+					<p class="body-2">
+						Copy <code>{copiedColour.value}</code> to your clipboard.
+					</p>
+				{/if}
+			</Toast>
+		{/if}
+	</div>
+{/key}
+
 <style>
-	p {
+	.body-4 {
 		margin-block: var(--space-5);
 	}
 
@@ -35,6 +75,13 @@
 		display: grid;
 		grid-template-columns: repeat(8, 1fr);
 		gap: var(--space-1);
+	}
+
+	.toaster {
+		z-index: 3;
+		position: fixed;
+		bottom: var(--space-10);
+		margin-inline: var(--space-10);
 	}
 
 	@media screen and (max-width: 1100px) {

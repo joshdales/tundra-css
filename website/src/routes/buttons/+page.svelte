dd2ca22008d85643bbb3e752e00068de0230ca7e
Josh Dales <git@joshdales.dev>
2023-12-07 13:19:28 -0500

    Replace he accent selector on the button page

--- website/src/routes/buttons/+page.svelte
+++ website/src/routes/buttons/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import AccentSelector from '$lib/components/accent_selector.svelte'
 	import CopyToaster from '$lib/components/copy_toaster.svelte'
 	import type { CopiedValue } from '$lib/components/copy_toaster.svelte'
 
@@ -36,50 +37,7 @@
 		a <code>button-*</code> class to change the size.
 	</p>
 
-	<fieldset name="accent">
-		<legend class="alt-heading-3">Accent</legend>
-
-		<label class:active={colour === 'red'} class="label-4" for="buttons-red">
-			Red
-			<input bind:group={colour} type="radio" name="buttons" value="red" id="buttons-red" hidden />
-		</label>
-
-		<label class:active={colour === 'blue'} class="label-4" for="buttons-blue">
-			Blue
-			<input
-				bind:group={colour}
-				type="radio"
-				name="buttons"
-				value="blue"
-				id="buttons-blue"
-				hidden
-			/>
-		</label>
-
-		<label class:active={colour === 'green'} class="label-4" for="buttons-green">
-			Green
-			<input
-				bind:group={colour}
-				type="radio"
-				name="buttons"
-				value="green"
-				id="buttons-green"
-				hidden
-			/>
-		</label>
-
-		<label class:active={colour === 'yellow'} class="label-4" for="buttons-yellow">
-			Yellow
-			<input
-				bind:group={colour}
-				type="radio"
-				name="buttons"
-				value="yellow"
-				id="buttons-yellow"
-				hidden
-			/>
-		</label>
-	</fieldset>
+	<AccentSelector selectedColour={colour} on:accent={(ev) => (colour = ev.detail)} />
 
 	<h2 class="alt-heading-4">Filled</h2>
 
@@ -131,48 +89,11 @@
 <CopyToaster {copiedValue} />
 
 <style>
-	.btn-group,
-	fieldset {
+	.btn-group {
 		display: grid;
 		grid-template-columns: repeat(4, 1fr);
 		gap: var(--space-6);
 		margin-block: var(--space-4);
-	}
-
-	fieldset {
-		border: none;
-		padding: 0;
-	}
-
-	legend {
-		margin-block-end: var(--space-4);
-	}
-
-	label {
-		cursor: pointer;
-		color: var(--foreground);
-		border-radius: var(--radius-2);
-		padding-block: var(--space-4);
-		padding-inline: var(--space-6);
-		border: 1.5px solid var(--neutral-6);
-		text-align: center;
-		transition:
-			all 0.1s ease-in-out,
-			transform 0.3s ease-in;
-	}
-
-	.active,
-	label:hover {
-		border-color: var(--blue-8);
-		background-color: oklch(from var(--blue-8) l c h / 40%);
-	}
-
-	label:hover {
-		box-shadow:
-			0 var(--space-1) var(--space-2) oklch(from var(--neutral-1) l c h / 5%),
-			0 0 var(--space-1) oklch(from var(--neutral-1) l c h / 10%),
-			inset var(--space-2) var(--space-1) var(--space-3) oklch(from var(--blue-7) l c h / 10%);
-		transform: translate(0, calc(var(--space-1) * -1));
 	}
 
 	button {

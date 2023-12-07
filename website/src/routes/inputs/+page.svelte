38ae1ef67d7bf10bee0606b3ce75041fc05dfba9
Josh Dales <git@joshdales.dev>
2023-12-07 13:22:50 -0500

    Add the accent selector to the inputs page

--- website/src/routes/inputs/+page.svelte
+++ website/src/routes/inputs/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+	import AccentSelector from '$lib/components/accent_selector.svelte'
+	let selectedColour = ''
+</script>
+
 <main>
 	<h1>Inputs (Text)</h1>
 
@@ -6,24 +11,26 @@
 		and combine it with the label class on the label element.
 	</p>
 
+	<AccentSelector {selectedColour} on:accent={(ev) => (selectedColour = ev.detail)} />
+
 	<label for="input1" class="label-1">
 		Input 1
-		<input class="input-1" type="text" placeholder="Input 1" id="input1" />
+		<input class="input-1 {selectedColour}" type="text" placeholder="Input 1" id="input1" />
 	</label>
 
 	<label for="input2" class="label-2">
 		Input 2
-		<input class="input-2" type="text" placeholder="Input 2" id="input2" />
+		<input class="input-2 {selectedColour}" type="text" placeholder="Input 2" id="input2" />
 	</label>
 
 	<label for="input3" class="label-3">
 		Input 3
-		<input class="input-3" type="text" placeholder="Input 3" id="input3" />
+		<input class="input-3 {selectedColour}" type="text" placeholder="Input 3" id="input3" />
 	</label>
 
 	<label for="input4" class="label-4">
 		Input 4
-		<input class="input-4" type="text" placeholder="Input 4" id="input4" />
+		<input class="input-4 {selectedColour}" type="text" placeholder="Input 4" id="input4" />
 	</label>
 </main>
 

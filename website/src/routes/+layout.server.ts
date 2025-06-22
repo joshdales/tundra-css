f053a2b8c8d1060d6a852c713751f1037c265afa
Josh Dales <git@joshdales.dev>
2025-06-22 16:45:02 -0400

    Pre-render what can

--- website/src/routes/+layout.server.ts
+++ website/src/routes/+layout.server.ts
@@ -1,6 +1,8 @@
 import type { Cookies } from '@sveltejs/kit'
 import type { Theme, AccentColour } from '$lib/theme.svelte'
 
+export const prerender = true
+
 export async function load({ cookies }: { cookies: Cookies }) {
 	const theme: Partial<Theme> = {}
 	const themeAccent = cookies.get('themeAccent')

67aeaca0a761bf0a899b447dc8c7de8eb20766a5
Josh Dales <git@joshdales.dev>
2025-11-16 10:48:52 -0500

    Update customisation classes

--- www/src/routes/customisation/+page.svelte
+++ www/src/routes/customisation/+page.svelte
@@ -1,7 +1,7 @@
-<main>
-	<h1 class="heading-6">Customisation</h1>
+<main class="text-body-4">
+	<h1 class="text-heading-6">Customisation</h1>
 
-	<p class="body-4">
+	<p>
 		I've tried to make this pretty easy to customise to get how you want with minimal fuss. For the
 		most part things that should be considered customisable are using CSS custom properties, though
 		you can always override whatever properties you want.
@@ -16,7 +16,7 @@
 		>.
 	</p>
 
-	<p class="body-4">
+	<p>
 		Generally the custom properties are named after the property that they are supposed to be
 		setting so that it is easy to guess what you need to change. There are a few of exceptions for
 		this though mostly around the padding of elements which goes for <code>--padding-x</code> and
@@ -24,8 +24,8 @@
 		<code>--padding-block</code>, but maybe I'll change that in the future.
 	</p>
 
-	<section class="body-4">
-		<h2 class="heading-5">Accent colours</h2>
+	<section>
+		<h2 class="text-heading-5">Accent colours</h2>
 		<p>
 			There are classes to applying the accent colour to whatever element you want, eg.
 			<code>red</code>, <code>blue</code>, <code>green</code>, etc. These all set the
@@ -61,7 +61,7 @@
 
 			There is more information about this in the
 			<a
-				class="external"
+				class="text-link-4 external"
 				href="https://github.com/joshdales/tundra-css/blob/main/README.md"
 				target="_blank"
 				rel="noopener noreferrer">project Readme</a
